c6b13e8fcf7df8ba85fd560523586d85adf9f5eb
marcin-wiatrak <marcin.wiatrak.95@gmail.com>
2021-02-23 00:11:45 +0100

    Added main wrapper styles and created navbar container

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,10 +1,25 @@
-import { GlobalStyle } from 'assets/styles/GlobalStyle';
 import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'assets/styles/GlobalStyle';
+import { theme } from 'assets/styles/theme';
+import Navbar from 'components/Layouts/Navbar/Navbar';
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+    color: ${({ theme }) => theme.colors.textPrimary};
+`;
 
 const App = () => (
-    <>
+    <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <h1>Global style</h1>
-    </>
+        <Navbar />
+        <Wrapper>
+            <h1>Global style</h1>
+        </Wrapper>
+    </ThemeProvider>
 );
 export default App;
