d497faa15888c42ae2289dcd8028bbc360785885
marcin-wiatrak <marcin.wiatrak.95@gmail.com>
2021-02-22 23:06:14 +0100

    First commit. + project set up + added global styles + font

---
+++ src/views/Root.js
@@ -0,0 +1,10 @@
+import { GlobalStyle } from 'assets/styles/GlobalStyle';
+import React from 'react';
+
+const App = () => (
+    <>
+        <GlobalStyle />
+        <h1>Global style</h1>
+    </>
+);
+export default App;
