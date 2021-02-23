b5f525194418558f47316e9a49f52ed5de298624
marcin-wiatrak <marcin.wiatrak.95@gmail.com>
2021-02-24 00:33:42 +0100

    Ranking created. Dynamicaly generated list od players

---
+++ src/data/Players.js
@@ -0,0 +1,22 @@
+export const Players = [
+    {
+        name: 'Marcin Wiatrak',
+        amount: 555010,
+    },
+    {
+        name: 'Liliana Wiatrak',
+        amount: 265588,
+    },
+    {
+        name: 'Hubert Frasz',
+        amount: 250000,
+    },
+    {
+        name: 'Bartosz Jaworowicz',
+        amount: 100000,
+    },
+    {
+        name: 'Amadeusz Rydlewski',
+        amount: 84000,
+    },
+];
